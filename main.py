54b9bf7afb5e6da322c3607d97ecbec092b17b6a
Fernnando Sussmann <fernnandoh@hotmail.com>
2018-04-17 15:09:41 -0300

    Cleaning and structuring data into dataframe

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ from pyspark.sql import Row
 def load_file(sc,file_path):
     return sc.textFile(file_path)
 
+def organize_data_into_dataframe(data_rdd):
+    from datetime import datetime
+    return data_rdd.map(lambda line: line.split(" ")) \
+            .map(lambda c: Row(host=c[0], 
+            timestamp=datetime.strptime(str(c[3].replace('[','')), '%d/%b/%Y:%H:%M:%S'), 
+            request=str(c[5]+c[6]).replace('"',''),
+            HTTPcode=int(c[8]), 
+            totalBytes=int(str(c[9]).replace('-','0')))).toDF()
+
+def data_frame_debug(df):
+    df.printSchema() 
+    df.show() 
+
 def main():
+    # from datetime import datetime
     sc = SparkContext()
     spark = SparkSession(sc)
 
     log_aug95_RDD = load_file(sc,"access_log_Aug95")
     log_jul95_RDD = load_file(sc,"access_log_Jul95")
 
-    log_aug95_DF = log_aug95_RDD.map(lambda line: line.split(" ")) \
-        .map(lambda c: Row(host=c[0], timestamp=c[3]+c[4], request=c[5]+c[6],\
-         HTTPcode=c[8], totalBytes=c[9])).toDF()
+    log_aug95_DF = organize_data_into_dataframe(log_aug95_RDD)
+    log_jul95_DF = organize_data_into_dataframe(log_jul95_RDD)
 
-    log_jul95_DF = log_jul95_RDD.map(lambda line: line.split(" ")) \
-        .map(lambda c: Row(host=c[0], timestamp=c[3]+c[4], request=c[5]+c[6],\
-         HTTPcode=c[8], totalBytes=c[9])).toDF()
-
-    log_aug95_DF.printSchema() 
-    log_jul95_DF.printSchema() 
-
-    log_aug95_DF.show() 
-    log_jul95_DF.show() 
-
+    data_frame_debug(log_aug95_DF)
+    data_frame_debug(log_jul95_DF)
 
 main()
